c978deaf10694c7401aeeb0d1999fa681689f1a5
2300030503 <2300030503@kluniversity.in>
2025-08-12 10:44:44 +0530

    Fix GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Ecommerce1/'
+  base: '/Ecommerce1/' // <-- this is very important
 });
